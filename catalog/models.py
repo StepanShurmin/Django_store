31106373393f2e60f0038a9d57dbe9a679a02f88
StepanShurmin <shurmin.sa@gmail.com>
2023-09-13 13:31:08 +0400

    Задания 2, 3.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='наименование')
+    product_description = models.CharField(max_length=200, verbose_name='описание продукта')
+    product_image = models.ImageField(upload_to='products/', verbose_name='изображение')
+    product_category = models.CharField(max_length=50, verbose_name='категория')
+    product_price = models.FloatField(verbose_name='цена')
+    product_date_creation = models.DateField(verbose_name='дата создания')
+    product_date_modification = models.DateField(verbose_name='дата последнего изменения')
+
+    is_active = models.BooleanField(default=True, verbose_name='в наличии')
+
+    def __str__(self):
+        return f'{self.product_name} {self.product_category} {self.product_price} {self.product_date_creation}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('product_category', 'product_name', 'product_date_modification')
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='наименование')
+    category_description = models.CharField(max_length=200, verbose_name='описание')
+
+    is_active = models.BooleanField(default=True, verbose_name='в наличии')
+
+    def __str__(self):
+        return f'{self.category_name} {self.category_description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('category_name',)
