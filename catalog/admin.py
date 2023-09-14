13a7ce5b6b7b54d0c1b9fb359a1fe24f159331bd
StepanShurmin <shurmin.sa@gmail.com>
2023-09-14 15:59:08 +0400

    Доделал.

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,12 +5,12 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category_name', 'category_description')
+    list_display = ('id', 'name', 'description')
     list_filter = ('is_active',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product_name', 'product_price', 'product_category')
-    list_filter = ('product_category', 'is_active')
-    search_fields = ('product_name', 'product_description')
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category', 'is_active')
+    search_fields = ('name', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -4,35 +4,35 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=50, verbose_name='наименование')
-    product_description = models.CharField(max_length=200, verbose_name='описание продукта', **NULLABLE)
-    product_image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
-    product_category = models.CharField(max_length=50, verbose_name='категория')
-    product_price = models.FloatField(verbose_name='цена')
-    product_date_creation = models.DateField(verbose_name='дата создания', **NULLABLE)
-    product_date_modification = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.CharField(max_length=200, verbose_name='описание продукта', **NULLABLE)
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name='категория')
+    price = models.FloatField(verbose_name='цена')
+    date_creation = models.DateField(verbose_name='дата создания', **NULLABLE)
+    date_modification = models.DateField(verbose_name='дата последнего изменения', **NULLABLE)
 
     is_active = models.BooleanField(default=True, verbose_name='в наличии')
 
     def __str__(self):
-        return f'{self.product_name} {self.product_category} {self.product_price} {self.product_date_creation}'
+        return f'{self.name} {self.category} {self.price} {self.date_creation}'
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
-        ordering = ('product_category', 'product_name', 'product_date_modification')
+        ordering = ('category', 'name', 'date_modification')
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, verbose_name='наименование')
-    category_description = models.CharField(max_length=200, verbose_name='описание')
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.CharField(max_length=200, verbose_name='описание')
 
     is_active = models.BooleanField(default=True, verbose_name='в наличии')
 
     def __str__(self):
-        return f'{self.category_name} {self.category_description}'
+        return f'{self.name} {self.description}'
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
-        ordering = ('category_name',)
+        ordering = ('name',)
