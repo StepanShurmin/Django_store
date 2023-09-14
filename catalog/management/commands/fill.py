5cd48f3f7b25eec49d3277ebf47632818e1f0567
StepanShurmin <shurmin.sa@gmail.com>
2023-09-14 16:03:07 +0400

    Доделал.

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,23 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+json_file = 'catalog/fixtures/data.json'
+
+
+def delete_objects(model):
+    """Чистит БД."""
+    model.objects.all().delete()
+
+
+def load_fixtures():
+    """Загружает данные в БД."""
+    call_command('loaddata', json_file)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for obj in [Product, Category]:
+            delete_objects(obj)
+
+        load_fixtures()
